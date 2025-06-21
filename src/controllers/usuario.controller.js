42796007adf20380047da05cb0c53ca376a1a11f
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2025-06-20 22:21:02 -0300

    Ajustes no cadastro

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,27 +1,38 @@
-const prisma = require('../prisma/client');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const secret = process.env.JWT_SECRET
+const prisma = require("../prisma/client");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
 
 module.exports.cadastrar = async (req, res) => {
-    try {
-        const { nome, sobrenome, email, genero, nascimento, usuario, tipo, senha} = req.body;
-        const nomeCompleto = nome + " " + sobrenome;
-        const senhaEncriptada = await bcrypt.hash(senha, 10);
-        
-        const novoUsuario = await prisma.usuario.create({
-            data: { nome, email, genero, nascimento, usuario, tipo, senha: senhaEncriptada, adm: false}
-        });
-        const payload = {
-            id: novoUsuario.id,
-            email: novoUsuario.email,
-            admin: novoUsuario.adm
-        };
-        const token = jwt.sign(payload, secret);
-        const usuarioEditado = await prisma.usuario.update({where: {id: novoUsuario.id}}, {data:{token}})
-        res.status(201).json(usuarioEditado);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Erro ao criar usuário.' });
-    }
+  try {
+    const { nome, sobrenome, email, genero, dataNascimento, usuario, senha } =
+      req.body;
+    const nomeCompleto = nome + " " + sobrenome;
+    const senhaEncriptada = await bcrypt.hash(senha, 10);
+    const novoUsuario = await prisma.usuario.create({
+      data: {
+        nome: nomeCompleto,
+        email,
+        genero,
+        nascimento: dataNascimento,
+        usuario,
+        senha: senhaEncriptada,
+        adm: false,
+      },
+    });
+    const payload = {
+      id: novoUsuario.id,
+      email: novoUsuario.email,
+      admin: novoUsuario.adm,
+    };
+    const token = jwt.sign(payload, secret);
+    const usuarioEditado = await prisma.usuario.update({
+      where: { id: novoUsuario.id },
+      data: { token },
+    });
+    res.status(201).json(usuarioEditado);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Erro ao criar usuário." });
+  }
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const router = require('./routers');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 dotenv.config()
