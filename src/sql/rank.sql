d71bed268d3f356615fa2eca121b9222f6b000ca
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2025-07-25 18:35:21 -0300

    ranks

---
+++ src/sql/rank.sql
@@ -0,0 +1,8 @@
+insert into "Rank"(nome, cor, tipo) values ('Iniciante Curioso', '#B0B0B0', 'RaciocinioLogico');
+insert into "Rank"(nome, cor, tipo) values ('Explorador de Variáveis', '#A8E6CF', 'Programacao');
+insert into "Rank"(nome, cor, tipo) values ('Operador Aprendiz', '#ADD8E6', 'Programacao');
+insert into "Rank"(nome, cor, tipo) values ('Condicional Criativo', '#FFD700', 'Programacao');
+insert into "Rank"(nome, cor, tipo) values ('Laçador de Repetições', '#FFA500', 'Programacao');
+insert into "Rank"(nome, cor, tipo) values ('Manipulador de Funções', '#C62828', 'Programacao');
+insert into "Rank"(nome, cor, tipo) values ('Domador de Dados', '#FF00BD', 'Programacao');
+insert into "Rank"(nome, cor, tipo) values ('Mestre da Lógica', '#6446DB', 'Programacao');
